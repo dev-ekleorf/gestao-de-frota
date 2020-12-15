3bb919272031b601eecd13010b084c3cc1e4b099
dev-ekleorf <ekleorf@gmail.com>
2020-12-15 01:30:44 -0300

    final

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './componentes/tela_login'
+import tela_login from './componentes/tela_login';
+
+function App() {
+  return (
+    <div className="App">
+      <tela_login></tela_login>
+    </div>
+
+      
+  );
+}
+
+export default App;
---
+++ src/pages/principal/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function principal(){
+return(
+    <p>Tela principal</p>
+
+);
+
+}
+export default principal;
